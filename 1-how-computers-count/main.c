afcbc7d883331b4fd22ecbe0fefb49e11dc8634d
Efraim Manurung <frm199911@gmail.com>
2024-02-05 00:39:43 +0100

    First commit

---
+++ 1-how-computers-count/main.c
@@ -0,0 +1,16 @@
+
+int main() { 
+    unsigned int counter = 0;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    
+    return 0;
+}
